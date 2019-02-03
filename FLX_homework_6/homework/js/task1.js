bb1063c83af14f7727098bd66f6cb8a9d78e0e66
Yura Prus <prys77777@gmail.com>
2019-02-03 19:54:07 +0200

    7

--- FLX_homework_6/homework/js/task1.js
+++ FLX_homework_6/homework/js/task1.js
@@ -1,3 +1,24 @@
-// Your code goes here
-
-console.log('dsds');
+function quadratic(){
+    var a = prompt ('enter: a');
+    var b = prompt ('enter: b');
+    var c = prompt ('enter: c');
+    var d = b*b-4*a*c;
+    var x = Math.floor(d);
+    if(x < 0){
+        (alert("no solution"));
+    }else if(a === "" || b === "" || c === ""){
+        (alert("Invalid input data"))
+    }else if(isNaN(a) || isNaN(b) || isNaN(c)){
+        (alert("Invalid input data"))
+    }else if(a === 0 || b === 0 || c === 0){
+        (alert("Invalid input data"))
+    }else if(x === 0){
+        (alert("x=0"));
+    }else if(x > 0){
+        var x1 = (-b-x)/(2*a);
+        var x2 = (-b+x)/(2*a); 
+        (alert("x1 =" + x1));
+        (alert("x2 =" + x2));
+    }  
+}
+quadratic();
--- FLX_homework_6/homework/js/task2.js
+++ FLX_homework_6/homework/js/task2.js
@@ -1,1 +1,29 @@
-// Your code goes here
+function moneyDiscount(){
+    var a = prompt ('enter money: ');
+    var b = prompt ('enter discount: ');
+    if(a <= 0){
+        (alert("Invalid input data"))
+    }else if(a > 9999999){
+        (alert("Invalid input data"))
+    }else if(a === ""){
+        (alert("Invalid input data"))
+    }else if(isNaN(a)){
+        (alert("Invalid input data"))
+    }else if(b < 0){
+        (alert("Invalid input data"))
+    }else if(b > 99){
+        (alert("Invalid input data"))
+    }else if(b === ""){
+        (alert("Invalid input data"))
+    }else if(isNaN(b)){
+        (alert("Invalid input data"))
+    }else if(a > 0 || a < 9999999 || b > 0 || b < 99){
+        var savemoney = a/100*b;
+        var saleprice = a-savemoney;
+        (alert("Price without discount: " + a+"$"));
+        (alert("Discount: " + b+"%"));
+        (alert("Price with discount: " + saleprice+"$"));
+        (alert("Saved:  " + savemoney+"$"));
+    }
+}
+moneyDiscount()
