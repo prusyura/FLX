e67f41f697c0ce88254cc1e2b1101e34b6c87e15
Yura Prus <prys77777@gmail.com>
2019-02-12 22:52:07 +0200

    11

--- FLX_homework_8/homework/js/formatTime.js
+++ FLX_homework_8/homework/js/formatTime.js
@@ -1,21 +1,21 @@
 function formatTime(time){
-    let day = 1440;
-    let hour = 60;
-    let minute = 1;
+    var day = 1440;
+    var hour = 60;
+    var minute = 1;
     while(time > 0){
         if(time > 1440){
-            let days = Math.floor(time / day);
+            var days = Math.floor(time / day);
             time = time - (days*day)
         }else if(time > 60){
-            let hours = Math.floor(time / hour);
+            var hours = Math.floor(time / hour);
             time = time - (hours*hour)
         }else if(time<60){
-            let minutes = Math.floor(time / minute);
+            var minutes = Math.floor(time / minute);
             time = time - (minutes*minute)
         }else {
             break;
         }
     }
-    return (this.days + ' day(s) ' + this.hours + ' hour(s) ' + this.minutes + ' minute(s).');
+    return (days + ' day(s) ' + hours + ' hour(s) ' + minutes + ' minute(s).');
 }
 formatTime(3601);
